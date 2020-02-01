9f8948ae7c1f4f127d96753c37a140e952ac5669
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-02-01 15:27:07 +0530

    Puggified
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-
+var app= express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('users', { title: 'AgriBazaar' });
