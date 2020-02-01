a232c76107012d95c7cece7742dac8be184640e4
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-02-01 14:49:52 +0530

    From Jade to Pug: Removing Jade because its long depreciated. THERE's NO DIFFERENCE BETWEEN JADE & PUG.
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

---
+++ routes/home.js
@@ -0,0 +1,8 @@
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
