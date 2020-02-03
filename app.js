12dbf050dfcd74217d5127f95369136111dfce70
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-02-03 14:31:50 +0530

    Backend Auth
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var express = require('express');
+var router = express.Router();
+var app= express();
+var session = require('express-session');
+
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,12 +24,11 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/users', usersRouter);	
+app.post('/users', user.login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,12 +46,42 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-module.exports = app;
-
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : '',
-	database : 'nodelogin'
+	password : 'nirmalkhedkar',
+	database : 'agribazaar'
+});
+connection.connect();
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+    var password = request.body.password;
+    console.log("AUTHENTICATION")
+	if (username && password) {
+		connection.query('SELECT * FROM Users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
 });
 
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -10,40 +10,4 @@ router.get('/', function(req, res, next) {
     res.render('users', { title: 'AgriBazaar' });
 });
 
-var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : 'nirmalkhedkar',
-	database : 'agribazaar'
-});
-
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
-
-app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
-		connection.query('SELECT * FROM Users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
-});
-
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
-
 module.exports = router;
