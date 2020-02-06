36b21a293e356f25ab52303c7669fcec78db7474
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-02-07 00:48:06 +0530

    COMMIT

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+
+router.get('/users', function(req, res, next) {
+   console.log(res)
+   res.send("USERS!")
 });
 
 module.exports = router;
